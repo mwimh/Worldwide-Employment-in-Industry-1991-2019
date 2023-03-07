2402c3703da697b30c53a18be4de14e9152d29cc
mwimh <112586751+mwimh@users.noreply.github.com>
2023-03-06 18:10:00 -0600

    Final Leaflet Lab
    Lab 1 Complete (Unit 2 Complete)

--- js/main.js
+++ js/main.js
@@ -17,6 +17,21 @@ function createMap() {
     getData();
 };
 
+// Create static informational items
+function createTitle() {
+    // Display Title
+    title.innerHTML = '<h1 class="title"><u>Global Shift of Industrial Employment (1991 - 2019)</u><br><font size="5">Top 20 <font color="#2FC9B9">Growing<font color="#000000"> and <font color="#D39631">Shrinking<font color="#000000"> Industrial Sectors Since 1991</h1>';
+    // Display Metadata
+    info.innerHTML = '<p class="info">Map Data from "The World Bank Group" © 2023</br>Supplemental information from Wikipedia: "Secondary sector of the economy"</br>Map Created by Michael Imhoff for UW-Madison Geography</p>'
+    // Display additional information
+    story.innerHTML = '<p class="story"><img src="img/indPhoto1.jpg" alt="" width="150" align="left" hspace="20px">The industrial sector includes fields that produce a finished, usable product from raw materials or are involved in construction. Manufacturing is an\
+    important activity in promoting economic growth and development. Nations that export manufactured products tend to generate higher marginal GDP growth, that supports higher incomes and therefore tax revenue needed to fund government expenditures like health \
+    care and infrastructure.</br></br><img src="img/indPhoto3.jpg" alt="" height="100" align="right" hspace="20px" vspace="10px">This map shows the 20 countries with the most growth and most decline since 1991. A noticable shift in employment in the industrial \
+    sector from Western countries to South-East Asia shows the out-sourcing of industry that has occured in the last 30 years. Also of note is significant growth in Qatar and Oman, due primarily to the growth of their Oil Industries.</p>'
+    // Display title over slider
+    sliderTitle.innerHTML = '<h1 class="sliderTitle">Year Selection (1991 - 2019)</h1>'
+};
+
 
 // Create the Popup Content Class and properties
 function PopupContent(properties, attribute) {
@@ -24,7 +39,9 @@ function PopupContent(properties, attribute) {
     this.attribute = attribute;
     this.year = attribute.split("_")[1];
     this.employment = properties[attribute];
+    // Calculate total change from 1991 to 2019
     this.change = Math.round((properties['ind_2019'] - properties['ind_1991']) * 10) / 10;
+    // If the change is positive, add a '+' sign to it
     if (this.change > 0) {
         this.change = "+" + this.change;
     }
@@ -32,21 +49,11 @@ function PopupContent(properties, attribute) {
 };
 
 
-function createTitle() {
-    title.innerHTML = '<h1 class="title"><u>Global Shift of Industrial Employment (1991 - 2019)</u><br><font size="5">Top 20 <font color="#2FC9B9">Growing<font color="#000000"> and <font color="#D39631">Shrinking<font color="#000000"> Industrial Sectors Since 1991</h1>';
-    info.innerHTML = '<p class="info">Map Data from "The World Bank Group" © 2023</br>Supplemental information from Wikipedia: "Secondary sector of the economy"</br>Map Created by Michael Imhoff for UW-Madison Geography</p>'
-    story.innerHTML = '<p class="story"><img src="img/indPhoto1.jpg" alt="" width="150" align="left" hspace="20px">The industrial sector includes fields that produce a finished, usable product from raw materials or are involved in construction. Manufacturing is an\
-    important activity in promoting economic growth and development. Nations that export manufactured products tend to generate higher marginal GDP growth, that supports higher incomes and therefore tax revenue needed to fund government expenditures like health \
-    care and infrastructure.</br></br><img src="img/indPhoto3.jpg" alt="" height="100" align="right" hspace="20px" vspace="10px">This map shows the 20 countries with the most growth and most decline since 1991. A noticable shift in employment in the industrial \
-    sector from Western countries to South-East Asia shows the out-sourcing of industry that has occured in the last 30 years. Also of note is significant growth in Qatar and Oman, due primarily to the growth of their Oil Industries.</p>'
-    sliderTitle.innerHTML = '<h1 class="sliderTitle">Year Selection (1991 - 2019)</h1>'
-};
-
-
+// Calculate min, max, and mean of the data
 function calcStats(data) {
     //create empty array to store all data values
     var allValues = [];
-    //loop through each city
+    //loop through each country
     for (var country of data.features) {
         //loop through each year
         for (var year = 1991; year <= 2019; year += 1) {
@@ -56,7 +63,7 @@ function calcStats(data) {
             allValues.push(value);
         }
     }
-    //get min, max, mean stats for our array
+    //get min, max, mean stats for the array
     dataStats.min = Math.min(...allValues);
     dataStats.max = Math.max(...allValues);
     //calculate meanValue
@@ -75,10 +82,11 @@ function calcPropRadius(attValue) {
 };
 
 
+// Create popup content HTML
 function createPopupContent(properties, attribute) {
-    //add Country to popup content string
+    // Add Country to popup content string
     var popupContent = "<p><b>Country:</b> " + properties.Country + "</p>";
-    //add formatted attribute to panel content string
+    // Add formatted attribute to panel content string
     var year = attribute.split("_")[1];
     popupContent += "<p><b>" + "Industry Emp. in " + year + ":</b> " + Math.round(properties[attribute] * 10) / 10 + "%</p>";
     return popupContent;
@@ -89,10 +97,9 @@ function createPopupContent(properties, attribute) {
 function pointToLayer(feature, latlng, attributes) {
     // Determine the attribute for scaling the proportional symbols
     var attribute = attributes[28];
-    //create new popup content...Example 1.4 line 1
+    // Create new popup content object
     var popupContent = new PopupContent(feature.properties, attribute);
-    //var popupContent = new PopupContent(feature.properties, attribute);
-    // Create marker options
+    // Set marker options
     var options = {
         fillColor: "#D39631",
         color: "#000",
@@ -110,7 +117,7 @@ function pointToLayer(feature, latlng, attributes) {
     options.radius = calcPropRadius(attValue);
     // Create circle marker layer
     var layer = L.circleMarker(latlng, options);
-    //bind the popup to the circle marker    
+    // Bind the popup to the circle marker    
     layer.bindPopup(popupContent.formatted, {
         offset: new L.Point(0, -options.radius)
     });
@@ -121,8 +128,8 @@ function pointToLayer(feature, latlng, attributes) {
 
 // Add circle markers for point features to the map
 function createPropSymbols(data, attributes) {
-    //create a Leaflet GeoJSON layer and add it to the map
-       L.geoJson(data, {
+    // Create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
         pointToLayer: function (feature, latlng) {
             return pointToLayer(feature, latlng, attributes);
         }
@@ -130,39 +137,56 @@ function createPropSymbols(data, attributes) {
 };
 
 
-//Create new sequence controls
+// Resize proportional symbols according to selected date
+function updatePropSymbols(attribute) {
+    // Create the 'year' variable
+    var year = attribute.split("_")[1];
+    // Update temporal legend
+    document.querySelector("span.year").innerHTML = year;
+    map.eachLayer(function (layer) {
+        if (layer.feature && layer.feature.properties[attribute]) {
+            // Update each feature's radius based on new attribute values
+            var props = layer.feature.properties
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+            // Add formatted attribute to panel content string
+            var popupContent = new PopupContent(props, attribute);
+            // Update popup with new content    
+            popup = layer.getPopup();
+            popup.setContent(popupContent.formatted).update();
+        };
+    });
+};
+
+
+// Create new sequence controls
 function createSequenceControls(attributes) {
     var SequenceControl = L.Control.extend({
         options: {
             position: 'bottomleft'
         },
-
+        // Set parameters for the slider
         onAdd: function () {
-            // create the control container with a particular class name
+            // Create the control container with a particular class name
             var container = L.DomUtil.create('div', 'sequence-control-container');
-
-            //create range input element (slider)
+            // Create range input element (slider) and add parameters
             container.insertAdjacentHTML('beforeend', '<input class="range-slider" type="range">')
-
             container.querySelector(".range-slider").max = 28;
             container.querySelector(".range-slider").min = 0;
             container.querySelector(".range-slider").value = 28;
             container.querySelector(".range-slider").step = 1;
-
-            //add skip buttons
+            // Add skip buttons and button images
             container.insertAdjacentHTML('beforeend', '<button class="step" id="reverse" title="Reverse"><img src="img/backarrow.png"></button>');
             container.insertAdjacentHTML('beforeend', '<button class="step" id="forward" title="Forward"><img src="img/forwardarrow.png"></button>');
-
-            //disable any mouse event listeners for the container
+            // Disable any mouse event listeners for the container
             L.DomEvent.disableClickPropagation(container);
-
+            // Return the contorl container
             return container;
         }
     });
 
-
-    map.addControl(new SequenceControl());    // add listeners after adding control}
-
+    // Add the sequence control
+    map.addControl(new SequenceControl());
     // Add functionality to the forward/reverse buttons
     document.querySelectorAll('.step').forEach(function (step) {
         step.addEventListener("click", function () {
@@ -183,7 +207,6 @@ function createSequenceControls(attributes) {
         })
     })
 
-
     // Add drag function to the slider
     document.querySelector('.range-slider').addEventListener('input', function () {
         // Get the new index value
@@ -194,64 +217,43 @@ function createSequenceControls(attributes) {
 }
 
 
-// Resize proportional symbols according to selected date
-function updatePropSymbols(attribute) {
-    var year = attribute.split("_")[1];
-    //update temporal legend
-    document.querySelector("span.year").innerHTML = year;
-    map.eachLayer(function (layer) {
-        if (layer.feature && layer.feature.properties[attribute]) {
-            // Update each feature's radius based on new attribute values
-            var props = layer.feature.properties
-            var radius = calcPropRadius(props[attribute]);
-            layer.setRadius(radius);
-            // Add formatted attribute to panel content string
-            var popupContent = new PopupContent(props, attribute);
-            //update popup with new content    
-            popup = layer.getPopup();
-            popup.setContent(popupContent.formatted).update();
-        };
-
-    });
-};
-
+// Create a legend
 function createLegend() {
     var LegendControl = L.Control.extend({
         options: {
             position: 'bottomright'
         },
-
+        // Set legend parameters and svg image
         onAdd: function () {
-            // create the control container with a particular class name
+            // Create the control container with a particular class name
             var container = L.DomUtil.create('div', 'legend-control-container');
             container.innerHTML = '<p class="temporalLegend">Percent Employed<br>in Industry in <span class="year">2019</span></p>';
-            //Step 1: start attribute legend svg string
+            // Start attribute legend svg string
             var svg = '<svg id="attribute-legend" width="190px" height="105px">';
-            //array of circle names to base loop on  
+            // Array of circle names to base loop on  
             var circles = ["max", "mean", "min"];
-            //Example 3.8 line 4...loop to add each circle and text to SVG string
+            // Loop to add each circle and text to SVG string
             for (var i = 0; i < circles.length; i++) {
-                //Step 3: assign the r and cy attributes            
+                // Assign the r and cy attributes            
                 var radius = calcPropRadius(dataStats[circles[i]]);
                 var cy = 100 - radius;
-                //circle string            
+                // Circle parameter string            
                 svg += '<circle class="legend-circle" id="' + circles[i] + '" r="' + radius + '"cy="' + cy + '" fill="#D3D3D3" fill-opacity="0.8" stroke="#000000" cx="65"/>';
-                //evenly space out labels            
+                // Evenly space out labels            
                 var textY = i * 42 + 12;
-                //text string            
+                // Text label string            
                 svg += '<text id="' + circles[i] + '-text" x="135" y="' + textY + '">' + Math.round(dataStats[circles[i]] * 10) / 10 + "%" + '</text>';
             };
-            //close svg string
+            // Close svg string
             svg += "</svg>";
 
-            //add attribute legend svg to container            
+            // Add attribute legend svg to container            
             container.insertAdjacentHTML('beforeend', svg);
             return container;
         }
     });
     map.addControl(new LegendControl());
 };
-
 
 
 // Build an attributes array from the data
@@ -273,24 +275,26 @@ function processData(data) {
 
 // Load and convert geojson data to be used
 function getData() {
-    // Load the data
+    // Load the data from the data folder
     fetch("data/IndustEmp.geojson")
         .then(function (response) {
             return response.json();
         })
+        // Call functions to create the map data
         .then(function (json) {
-            // Calculate minimum data value
             var attributes = processData(json);
-            // Call function to create proportional symbols
+            // Calculate min, max, and mean
             calcStats(json);
-            createPropSymbols(json, attributes);            
+            // Call function to create proportional symbols 
+            createPropSymbols(json, attributes);
             // Call function to create controls
             createSequenceControls(attributes);
+            // Call function to create the legend
             createLegend(attributes);
+            // Add informational panels and titles to the map
             createTitle();
         })
 };
-
 
 // Create the map once everything is loaded
 document.addEventListener('DOMContentLoaded', createMap);
